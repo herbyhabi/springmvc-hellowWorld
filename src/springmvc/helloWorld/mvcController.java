c363c07b2036178a787f2a07b00db2a90d26b00a
herby <2763431590@qq.com>
2019-06-29 19:26:21 +0800

    Initial commit

---
+++ src/springmvc/helloWorld/mvcController.java
@@ -0,0 +1,15 @@
+package springmvc.helloWorld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/mvc")
+public class mvcController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
